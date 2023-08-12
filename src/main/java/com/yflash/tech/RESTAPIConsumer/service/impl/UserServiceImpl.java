a5416e2a9865b0e0b0666fc2f1551f6ea9d640aa
samyak <samyakagarwal20@gmail.com>
2023-08-12 23:10:24 +0530

    adding changes for retry with recover

--- src/main/java/com/yflash/tech/RESTAPIConsumer/service/impl/UserServiceImpl.java
+++ src/main/java/com/yflash/tech/RESTAPIConsumer/service/impl/UserServiceImpl.java
@@ -10,12 +10,17 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -34,12 +39,14 @@ public class UserServiceImpl implements UserService {
     }
 
     private static final Logger LOGGER = LogManager.getLogger(UserServiceImpl.class);
+    private int attemptCount = 1;
+    private boolean attemptStatus = false;
 
     @Override
-    @Retryable(label = "retry-getAllUsers()", maxAttempts = 4, backoff = @Backoff(delay = 2000))
+    @Retryable(label = "retry-getAllUsers()", maxAttempts = 4, backoff = @Backoff(delay = 1000))
     public List<User> getAllUsers() throws Exception {
         List<User> result = null;
-        try{
+        try {
             LOGGER.info("Preparing the request ...");
             String wsUrl = environment.getProperty("producer.api.url");
             LOGGER.info("\t|--- Setting up the headers");
@@ -48,21 +55,35 @@ public class UserServiceImpl implements UserService {
 
             LOGGER.info("\t|--- Setting up the request body");
             HttpEntity<String> reqEntity = new HttpEntity<>(headers);
-            ParameterizedTypeReference<List<User>> responseType = new ParameterizedTypeReference<List<User>>() {
-            };
+            ParameterizedTypeReference<List<User>> responseType = new ParameterizedTypeReference<List<User>>() {};
 
             LOGGER.info("Fetching users data ...");
             ResponseEntity<List<User>> response = restTemplate.exchange(wsUrl, HttpMethod.GET, reqEntity, responseType);
 
             if (response.getStatusCode().is2xxSuccessful()) {
+                attemptStatus = true;
                 result = response.getBody();
                 LOGGER.info("Data fetched successfully !");
             }
-        } catch (Exception e) {
-            LOGGER.error("Error in getAllUsers() by {} : {}", e.getClass().getCanonicalName(), e.getMessage());
+        } catch (HttpClientErrorException httpClientErrorException){
+            LOGGER.error("Error in getAllUsers() due to {} : {}", httpClientErrorException.getClass().getCanonicalName(), httpClientErrorException.getMessage());
+            throw new SQLException(httpClientErrorException.getMessage());
+        }
+        catch (Exception e) {
+            LOGGER.error("Error in getAllUsers() due to {} : {}", e.getClass().getCanonicalName(), e.getMessage());
             throw new Exception(e.getMessage());
+        } finally {
+            LOGGER.info("Retry attempt {} completed : {}\n", attemptCount++, attemptStatus ? "success" : "error");
         }
         return result;
+    }
+
+    @Recover
+    public List<User> recover(Throwable throwable) {
+        LOGGER.error("Encountered exception {} : {}", throwable.getClass().getCanonicalName(), throwable.getMessage());
+        LOGGER.info("Max number of retry attempts exhausted.");
+        LOGGER.info("Please try again in sometime...");
+        return new ArrayList<>();
     }
 
 }
